ea56151764015ecd4883747b7d777be5580683f4
Ralf Stephan <ralf@ark.in-berlin.de>
2024-06-21 19:44:26 +0200

    bump

--- euler-E026a.lean
+++ euler-E026a.lean
@@ -2,18 +2,8 @@ import Mathlib
 
 set_option autoImplicit false
 set_option relaxedAutoImplicit false
-set_option linter.unusedVariables false
 
 open Nat
-
-lemma two_pow_isprimepower {m n : ℕ} (h1 : 0 < m) (h2 : 2 ^ m = n) : IsPrimePow n :=
-  ⟨2, m, Nat.prime_two.prime, h1, h2⟩
-
-lemma ne_iff_add_self {a b c : ℕ}: a ≠ b ↔ a + c ≠ b + c := by omega
-
-lemma ne_self_pow (a n : ℕ) (ha : 1 < a) (hn : 1 < n): a ≠ a ^ n := by
-  rw [ne_iff_lt_or_gt]
-  exact Or.inl <| lt_self_pow ha hn
 
 lemma simplify1 (a m' : ℕ) (ha : a > 0) :
     a ^ (2 * m' + 1) + 1 + (a + 1) * (a ^ (2 * m' + 2) - a ^ (2 * m' + 1)) =
@@ -73,20 +63,22 @@ lemma ord_compl_eq_or_lt (n p : ℕ) (hn : 0 < n) :
   · apply ord_compl_le n
   · apply ord_compl_pos p (not_eq_zero_of_lt hn)
 
-/- With respect to prime `p`, the odd part of `p ^ m` is 1. -/
+/- With respect to prime `p`, the odd part of `p ^ m` is 1 only if `n` is a power of `p`. -/
 lemma ord_compl_of_pow (m n p: ℕ) (hp : p.Prime) (hn : n = p ^ m) : ord_compl[p] n = 1 := by
   rw [hn, Prime.factorization_pow, Finsupp.single_eq_same]
   simp only [Prime.pos hp, ofNat_pos, pow_pos, Nat.div_self]
   exact hp
 
-lemma ord_pow_of_compl (n p: ℕ) (hp : p.Prime) (hnop : ord_compl[p] n = 1)
+/- `n` is a power of `p` only if the odd part of `p ^ m` is 1. -/
+lemma ord_pow_of_compl (n p: ℕ) (hnop : ord_compl[p] n = 1)
     : ∃ m : ℕ, n = p ^ m := by
   have h : p ^ n.factorization p * (n / p ^ n.factorization p) = n := ord_proj_mul_ord_compl_eq_self n p
   use n.factorization p
   rw [hnop, mul_one] at h
   exact h.symm
 
-lemma simplify2 (n : ℕ) (hn : n ≠ 0): (ord_proj[2] n) * (2 * ((ord_compl[2] n - 1) / 2) + 1) = n :=
+lemma simplify2 (n : ℕ) (hn : n ≠ 0) :
+    (ord_proj[2] n) * (2 * ((ord_compl[2] n - 1) / 2) + 1) = n :=
   calc
     (ord_proj[2] n) * (2 * ((ord_compl[2] n - 1) / 2) + 1) =
         (ord_proj[2] n) * ((ord_compl[2] n - 1) + 1) := by
@@ -94,7 +86,7 @@ lemma simplify2 (n : ℕ) (hn : n ≠ 0): (ord_proj[2] n) * (2 * ((ord_compl[2] 
       apply Nat.Odd.sub_odd _ (odd_iff.mpr (by rfl))
       exact odd_iff.mpr <| two_dvd_ne_zero.mp <| not_dvd_ord_compl prime_two hn
     _ = (ord_proj[2] n) * (ord_compl[2] n) := by
-      have (n : ℕ) (hn : 1 ≤ n) (h : Odd n) : Even (n - 1) := by
+      have (n : ℕ) (h : Odd n) : Even (n - 1) := by
         exact Nat.Odd.sub_odd h (odd_iff.mpr (by rfl))
       rw [Nat.sub_add_cancel <| zero_lt_of_lt <| ord_compl_pos 2 hn]
     _ = n := by
@@ -106,7 +98,7 @@ theorem H3 (a n : ℕ) (ha : 1 < a) (hn : 1 < n)
     (hP : Nat.Prime (a ^ n + 1)) : ∃ m : ℕ, n = 2 ^ m := by
   /- First we show that the goal `n = 2 ^ m` is equivalent to the statement
      "the odd part of `n` is 1", which is written in Lean using the `factorization` function. -/
-  apply ord_pow_of_compl n 2 prime_two
+  apply ord_pow_of_compl n 2
   /- The goal is now to show that the odd part of `n` must be 1.
      The odd part of any number is either 1 (which is equivalent to being a power
      of two), or greater than one and less or equal `n`. This is hypothesis `h₁`
@@ -122,32 +114,46 @@ theorem H3 (a n : ℕ) (ha : 1 < a) (hn : 1 < n)
      second proposition in `h₁`. -/
   case hB
   /- In this part we prove that setting `n` as of form `m₁ * (2 * m₂ + 1)`, `m₁, m₂ > 0`
-     (the current goal) leads to a contradiction. However, first the goal is to be
-     rewritten to match the lemma `H2` we proved earlier. -/
+     (the current goal) leads to a contradiction. -/
   · by_contra hB'
     rcases hB' with ⟨ h₂, _ ⟩
+    /- This extracts h₂ from hB'. It states that the odd part of `n` is greater 1.
+       This will be essential for the following arguments. -/
     apply Iff.mpr (Nat.not_prime_iff_exists_dvd_ne (by exact Prime.two_le hP))
+    /- This sets up the contradiction with the hypothesis `hP` that `a^n+1` be prime.
+       From assuming `a^n+1` is not prime follows that we need to prove a proper
+       divisor exists, creating the subgoals that it divides `a^n+1`, that it is
+       not 1, and that it is not equal to `a^n+1`. -/
     . use a ^ ord_proj[2] n + 1
+      /- We set `a^p+1` as the divisor, with `p` the even part of `n`. -/
       constructor
+      /- The constructor splits off the first subgoal. -/
       . nth_rewrite 2 [← simplify2 n (not_eq_zero_of_lt hn)]
+        /- We rewrite the divisor to match the lemma `H2` we proved earlier. -/
         apply H2 a (m := (ord_compl[2] n - 1) / 2) (p := ord_proj[2] n)
         exact zero_lt_of_lt ha
+        /- And we applied lemma `H2` to prove our divisor. -/
       . constructor
+        /- The constructor splits off the second and third subgoal. -/
         . rw [add_left_ne_self]
           apply pow_ne_zero
           exact not_eq_zero_of_lt ha
-        . rw [ne_eq, add_right_cancel_iff]
+          /- With this we proved our divisor is not 1. -/
+        . /- To show that the divisor does not equal `a^n+1` it suffices to show
+            that the even part of `n` is not equal to `n`, i.e., that `n` is not
+            a power of two. But we have excluded that possibility by setting
+            `n` as of form `m₁ * (2 * m₂ + 1)`, `m₁, m₂ > 0`, which is `h₂`.
+            To realize this argument, we just need to rewrite `h₂` until it
+            matches the goal. -/
+          rw [ne_eq, add_right_cancel_iff]
           rw [pow_right_inj (a := a) (m := ord_proj[2] n) (n := n)
             (zero_lt_of_lt ha) (Ne.symm (Nat.ne_of_lt ha))]
           rw [← ne_eq]
           rw [← mul_lt_mul_left (b := 1) (c := ord_compl[2] n)
-            (a := ord_proj[2] n)] at h₂
+            (a := ord_proj[2] n) (ord_proj_pos n 2)] at h₂
           rw [← Nat.mul_comm (n / ord_proj[2] n) (ord_proj[2] n)] at h₂
-          rw [Nat.div_mul_cancel, mul_one] at h₂
-          apply Nat.ne_of_lt at h₂
-          . exact h₂
-          . exact ord_proj_dvd n 2
-          . exact ord_proj_pos n 2
+          rw [Nat.div_mul_cancel (ord_proj_dvd n 2), mul_one] at h₂
+          exact Nat.ne_of_lt h₂
     . exact hP
   /- Having excluded the other case, only the possibility remains that `n` is a
     power of two. -/
